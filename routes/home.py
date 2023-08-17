b4002357a60bb14068ebc99e14829bfbef8314bc
Ahni Anderson <ahnianderson@Ahnis-MBP.attlocal.net>
2023-08-17 16:20:27 -0400

    add ConAdsP1 phone number posting

--- routes/home.py
+++ routes/home.py
@@ -203,7 +203,7 @@ def add_data():
             ]
 
             # Append phone_number and lead_cost together for specific labels
-            if label in ['IQ Media', 'Spot Tower', 'Top10']:
+            if label in ['IQ Media', 'Spot Tower', 'Top10', 'ConAdsP1']:
                 values_to_append.extend([phone_number, str(lead_cost)])
             else:
                 values_to_append.append(str(lead_cost))

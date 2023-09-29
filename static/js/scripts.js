83cf08235699a9bfca5731f57966373dc1494dc8
Ahni Anderson <ahnianderson@Ahnis-MBP.attlocal.net>
2023-09-29 16:30:51 -0400

    change settings modal css

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -57,113 +57,83 @@ function showTitle(element) {
 
 
 // DOMAINS SETTINGS
- // Get the modal
- var modal = document.getElementById("settingsModal");
 
- // Get the <span> element that closes the modal
- var closeBtn = document.getElementsByClassName("close")[0];
+var modal = $("#settingsModal");
 
- // Get the form element
- var form = document.getElementById("settingsForm");
+// Iterate over the settings buttons and add click event listeners
+$(".settings-button").click(function() {
+    var label = $(this).attr("data-label");
 
- // Iterate over the settings buttons and add click event listeners
- var settingsButtons = document.querySelectorAll(".settings-button");
- settingsButtons.forEach(function (button) {
-     button.addEventListener("click", function () {
-         var label = this.getAttribute("data-label");
+    // Get current domain info from the server
+    $.get('/get_domain_info/' + label, function(domainInfo) {
+        console.log("Received domain info:", domainInfo);
+        // Set the values in the modal form
+        $("#labelInput").val(domainInfo.label);
+        $("#leadCostInput").val(domainInfo.lead_cost);
+        $("#domainInput").val(domainInfo.domain);
+        $("#phoneInput").val(domainInfo.d_phone_number);
+        $("#sendToLeadsAPI").prop('checked', domainInfo.send_to_leads_api == "1");
+        $("#sendToGoogleSheet").prop('checked', domainInfo.send_to_google_sheet == "1");
+        $("#twilioNumberValidation").prop('checked', domainInfo.twilio_number_validation == "1");
+        $("#moverref").val(domainInfo.moverref);
+        $("#smsTexting").prop('checked', domainInfo.sms_texting == "1");
+        $("#changeMoverRefInput").prop('checked', domainInfo.change_moverref == 1);
+    });
 
-         // Get current domain info from the server
-         $.get('/get_domain_info/' + label, function (domainInfo) {
-             console.log("Received domain info:", domainInfo);
-             // Set the values in the modal form
-             document.getElementById("labelInput").value = domainInfo.label;
-             document.getElementById("leadCostInput").value = domainInfo.lead_cost;
-             document.getElementById("domainInput").value = domainInfo.domain;
-             document.getElementById("phoneInput").value = domainInfo.d_phone_number;
-             document.getElementById("sendToLeadsAPI").checked = (domainInfo.send_to_leads_api == "1");
-             document.getElementById("sendToGoogleSheet").checked = (domainInfo.send_to_google_sheet == "1");
-             document.getElementById("twilioNumberValidation").checked = (domainInfo.twilio_number_validation == "1");
-             document.getElementById("moverref").value = domainInfo.moverref;
-             document.getElementById("smsTexting").checked = (domainInfo.sms_texting == "1");
-             document.getElementById("changeMoverRefInput").checked = domainInfo.change_moverref == 1;
-         });
+    modal.modal('show');
+});
 
-         modal.style.display = "block";
-     });
- });
-
- // Close the modal when the close button is clicked
- closeBtn.addEventListener("click", function () {
-     modal.style.display = "none";
- });
-
- // Close the modal when the user clicks outside the modal content
- window.addEventListener("click", function (event) {
-     if (event.target == modal) {
-         modal.style.display = "none";
-     }
- });
-
- form.addEventListener("submit", function (event) {
+$("#settingsForm").submit(function(event) {
     event.preventDefault();
 
     // Retrieve the form field values
-    var label = document.getElementById("labelInput").value;
-    var domain = document.getElementById("domainInput").value;
-    var phone = document.getElementById("phoneInput").value;
-    var sendToLeadsAPI = document.getElementById("sendToLeadsAPI").checked;
-    var sendToGoogleSheet = document.getElementById("sendToGoogleSheet").checked;
-    var twilioNumberValidation = document.getElementById("twilioNumberValidation").checked;
-    var smsTexting = document.getElementById("smsTexting").checked;
-    var leadCost = document.getElementById("leadCostInput").value;
-    var changeMoverRef = document.getElementById("changeMoverRefInput").checked;
-    var moverref = document.getElementById("moverref").value; // Add this line to get the moverref value
+    var label = $("#labelInput").val();
+    var domain = $("#domainInput").val();
+    var phone = $("#phoneInput").val();
+    var sendToLeadsAPI = $("#sendToLeadsAPI").is(':checked');
+    var sendToGoogleSheet = $("#sendToGoogleSheet").is(':checked');
+    var twilioNumberValidation = $("#twilioNumberValidation").is(':checked');
+    var smsTexting = $("#smsTexting").is(':checked');
+    var leadCost = $("#leadCostInput").val();
+    var changeMoverRef = $("#changeMoverRefInput").is(':checked');
+    var moverref = $("#moverref").val();
 
     // Perform an AJAX request to update the domain properties on the server
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "/update_domain/" + label, true);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-            // Handle the response if needed
+    $.ajax({
+        type: "POST",
+        url: "/update_domain/" + label,
+        data: {
+            original_label: label,
+            label: label,
+            domain: domain,
+            phone_number: phone,
+            send_to_leads_api: sendToLeadsAPI ? "1" : "0",
+            send_to_google_sheet: sendToGoogleSheet ? "1" : "0",
+            twilio_number_validation: twilioNumberValidation ? "1" : "0",
+            moverref: moverref,
+            sms_texting: smsTexting ? "1" : "0",
+            lead_cost: leadCost,
+            change_moverref: changeMoverRef ? "1" : "0"
+        },
+        success: function(response) {
             console.log("Domain settings updated successfully");
-            modal.style.display = "none";
+            modal.modal('hide');
+        },
+        error: function(xhr, status, error) {
+            console.error("Error updating domain settings:", error);
         }
-    };
-    var params =
-        "original_label=" +
-        encodeURIComponent(label) +
-        "&label=" +
-        encodeURIComponent(label) +
-        "&domain=" +
-        encodeURIComponent(domain) +
-        "&phone_number=" +
-        encodeURIComponent(phone) +
-        "&send_to_leads_api=" +
-        (sendToLeadsAPI ? "1" : "0") +
-        "&send_to_google_sheet=" +
-        (sendToGoogleSheet ? "1" : "0") +
-        "&twilio_number_validation=" +
-        (twilioNumberValidation ? "1" : "0") +
-        "&moverref=" + encodeURIComponent(moverref) +
-        "&sms_texting=" +
-        (smsTexting ? "1" : "0") +
-        "&lead_cost=" + 
-        encodeURIComponent(leadCost) + 
-        "&change_moverref=" + 
-        (changeMoverRef ? "1" : "0");
-    xhr.send(params);
+    });
 });
 
- document.getElementById("addDomainButton").addEventListener("click", function() {
-   this.style.display = "none";
-   document.getElementById("addDomainForm").style.display = "block";
- });
- document.getElementById("cancelButton").addEventListener("click", function() {
-   document.getElementById("addDomainButton").style.display = "block";
-   document.getElementById("addDomainForm").style.display = "none";
- });
-// END DOMAIN SETTINGS
+$("#addDomainButton").click(function() {
+    $(this).hide();
+    $("#addDomainForm").show();
+});
+
+$("#cancelButton").click(function() {
+    $("#addDomainButton").show();
+    $("#addDomainForm").hide();
+});
 
 // password reset link popup
 $(document).ready(function() {
